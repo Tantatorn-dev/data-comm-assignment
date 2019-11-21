519d29d4a6f5362dc76d43db7e562ad64fc11ea0
dnjooiopa <dnjooiopa@gmail.com>
2019-11-21 22:32:12 +0700

    initial commit from process_image

---
+++ process_image/gen_image.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+class MyImage:
+
+    def __init__(self, dim):
+        self.dim = dim
+        self.pixel = dim[0] * dim[0]
+        self.lable = ['top', 'bottom', 'left', 'right', 'upper', 'lower']
+        self.image = []
+        for i in range(6):
+            tempImg = self.gen6Img(dim, self.lable[i])
+            self.image.append(tempImg)
+
+    def getOriginal(self, img_lable):
+        orig = "Not found"
+        for i in range(6):
+            if self.lable[i] == img_lable:
+                orig = self.image[i]
+        return orig
+
+    def predict(self, img, ERROR_THRESHOLD):
+        results = []
+        keyResults = {}
+        for i in range(6):
+            print(self.lable[i], end=": ")
+            matches = self.imgCheck(img, self.image[i])
+            results.append(matches)
+            keyResults[self.lable[i]] = matches
+            print("matches", matches, "of", str(self.pixel))
+        maxVal = max(results)
+        maxi = results.index(maxVal)
+        predicted_image = self.lable[maxi]
+        if maxVal < ERROR_THRESHOLD:
+            predicted_image = "error"
+        return results, keyResults, predicted_image
+
+
+    def imgCheck(self, img1, img2):
+        lenght = img1.shape[0]
+        matches = 0
+        for y in range(lenght):
+            for x in range(lenght):
+                if img1[y][x] == img2[y][x]:
+                    matches += 1;
+        return matches
+
+    def gen6Img(self, dimention, type):
+        buff = np.zeros(dimention, np.uint8)
+        row = dimention[0]
+        col = dimention[1]
+        half = row // 2
+        for r in range(row):
+            for c in range(col):
+                check = False
+                if type == "top":
+                    if r+1 <= half:
+                        check = True
+                if type == "bottom":
+                    if r+1 > half:
+                        check = True
+                if type == "left":
+                    if c+1 <= half:
+                        check = True
+                if type == "right":
+                    if c+1 > half:
+                        check = True
+                if type == "upper":
+                    if c >= r:
+                        check = True
+                if type == "lower":
+                    if r >= c:
+                        check = True
+                if check:
+                    buff[r][c] = 255
+        return buff
---
+++ process_image/process_img.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from gen_image import MyImage
+
+
+path = 'C:\\out'
+
+fileName = input("Enter file name(.bmp): ")
+
+image = cv2.imread(path + "\\" + fileName + ".bmp")
+print('original image shape' + str(image.shape))
+
+blur = cv2.GaussianBlur(image,(5,5),0)
+rotated = cv2.rotate(blur, cv2.ROTATE_180)
+gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
+print('gray image shape: ' + str(gray.shape))
+
+# mapping useful for low brightness image
+def mapImg(img):
+    scale = np.amax(img) - np.amin(img)
+    rows = img.shape[0]
+    cols = img.shape[1]
+    newImg = np.zeros((rows, cols), np.uint8)
+    for r in range(rows):
+        for c in range(cols):
+            newVal = np.uint8(((img[r][c]-np.amin(img)) / scale) * 255)
+            newImg[r][c] = newVal
+    return newImg
+
+n = 4
+mapped = mapImg(cv2.resize(gray, (gray.shape[1] // n,gray.shape[0] // n)))
+
+threshold_value = 80
+retVal, thresh = cv2.threshold(mapped, threshold_value, 255, cv2.THRESH_BINARY)
+print('thresh image shape: ' + str(thresh.shape))
+
+x = 24
+dim = (x, x)
+resized = cv2.resize(thresh, dim)
+retValV2, thresh24 = cv2.threshold(resized, 120, 255, cv2.THRESH_BINARY)
+
+dim4 = (4,4)
+resize4 = cv2.resize(thresh24, dim4)
+
+ret, thresh4 = cv2.threshold(resize4, 150, 255, cv2.THRESH_BINARY)
+
+
+# Predicting image
+images = MyImage(dim)
+
+results, key, predicted = images.predict(thresh24, 400)
+print(results)
+print(key)
+print("The image found is", end=" ")
+print(predicted)
+
+if predicted != "error":
+    orig = images.getOriginal(predicted)
+    
+
+# pyplot give number of rows, columns and index
+rows = 3
+cols = 3
+pos = (rows*100) + (cols*10)
+
+plt.subplot(pos+1),plt.imshow(image, cmap = 'gray')
+plt.title('image'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(pos+2),plt.imshow(rotated, cmap = 'gray')
+plt.title('rotated'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(pos+3),plt.imshow(mapped, cmap = 'gray')
+plt.title('after mapping'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(pos+4),plt.imshow(thresh, cmap = 'gray')
+plt.title('Thresh'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(pos+5),plt.imshow(resized, cmap = 'gray')
+plt.title('Resized'), plt.xticks([]), plt.yticks([])
+ 
+plt.subplot(pos+6),plt.imshow(thresh24, cmap = 'gray')
+plt.title('thresh24'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(pos+7),plt.imshow(resize4, cmap = 'gray')
+plt.title('thresh 16 pixel'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(pos+8),plt.imshow(thresh4, cmap = 'gray')
+plt.title('thresh 16 pixel v 2'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(pos+9),plt.imshow(orig, cmap = 'gray')
+plt.title('original image'), plt.xticks([]), plt.yticks([])
+
+plt.show()

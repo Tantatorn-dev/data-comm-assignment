6d4365d3b6f2718a9f5ba5429fd0c6f411fe8719
Kittisak <dnjooiopa@gmail.com>
2019-11-22 20:39:28 +0700

    jpg process

--- process_image/image.py
+++ process_image/image.py
@@ -5,49 +5,44 @@ import numpy as np
 from my_image import MyImage
 
 def getLatestFile(path):
-    path = "C:\\out"
+    
     os.chdir(path)
     files = os.listdir(path)
     files.sort(key=lambda x: os.path.getmtime(x))
     return files[-1]
 
 def preProcessImage():
-    # get latest image
-    path = 'C:\\out'
-    fileName = getLatestFile(path)
-    print("Latest file is", fileName)
-    #fileName = "out92.bmp"
-    image = cv2.imread(path + "\\" + fileName)
 
-    # reduce noise and convert to gray scale
+    path = "C:\\out"
+    fileName = input("Enter fileName(.jpg): ") + ".jpg"
+    image = cv2.imread((path + "\\" + fileName), 0)
+
+    # reduce noise
     blur = cv2.GaussianBlur(image, (5,5), 0)
-    rotated = cv2.rotate(blur, cv2.ROTATE_180)
-    gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
 
     # mapping useful for low brightness image
     def mapImg(img):
         scale = np.amax(img) - np.amin(img)
-        rows = img.shape[0]
-        cols = img.shape[1]
+        rows, cols = img.shape
         newImg = np.zeros((rows, cols), np.uint8)
         for r in range(rows):
-            for c in range(cols):
+             for c in range(cols):
                 newVal = np.uint8(((img[r][c]-np.amin(img)) / scale) * 255)
                 newImg[r][c] = newVal
         return newImg
-    mapped = mapImg(gray)
 
-    # first threshold for mapped image
-    retVal, thresh = cv2.threshold(mapped, 80, 255, cv2.THRESH_BINARY) 
+    mapped = mapImg(blur)
 
     # second threshold for 24 x 24 image
     dim24 = (24, 24)
-    resize24 = cv2.resize(thresh, dim24)
-    retValV2, thresh24 = cv2.threshold(resize24, 150, 255, cv2.THRESH_BINARY)
+    resize24 = cv2.resize(mapped, dim24)
+
+    retValV2, thresh24 = cv2.threshold(resize24, 80, 255, cv2.THRESH_BINARY)
 
     # final threshold for 4 x 4 image
     dim4 = (4,4)
     resize4 = cv2.resize(thresh24, dim4)
+
     ret, thresh4 = cv2.threshold(resize4, 150, 255, cv2.THRESH_BINARY)
 
     img24 = thresh24
--- process_image/process_img.py
+++ process_image/process_img.py
@@ -1,49 +1,64 @@
 import os
 import numpy as np
 import cv2
+import math
 from matplotlib import pyplot as plt
 from my_image import MyImage
 
 
 path = 'C:\\out'
 
-fileName = input("Enter file name(.bmp): ")
+fileName = input("Enter file name(.jpg): ") + ".jpg"
 
-image = cv2.imread(path + "\\" + fileName + ".bmp")
-print('original image shape' + str(image.shape))
+imgStacks = []
 
-blur = cv2.GaussianBlur(image,(5,5),0)
-rotated = cv2.rotate(blur, cv2.ROTATE_180)
-gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
+image = cv2.imread((path + "\\" + fileName), 0)
+
+imgStacks.append(("original", image))
+
+# reduce noise
+blur = cv2.GaussianBlur(image, (5,5), 0)
+imgStacks.append(("blur", blur))
 
 # mapping useful for low brightness image
 def mapImg(img):
     scale = np.amax(img) - np.amin(img)
     rows, cols = img.shape
     newImg = np.zeros((rows, cols), np.uint8)
+
     for r in range(rows):
-        for c in range(cols):
+         for c in range(cols):
             newVal = np.uint8(((img[r][c]-np.amin(img)) / scale) * 255)
             newImg[r][c] = newVal
     return newImg
 
-mapped = mapImg(gray)
+mapped = mapImg(blur)
+imgStacks.append(("after mapping", mapped))
 
-retVal, thresh = cv2.threshold(mapped, 80, 255, cv2.THRESH_BINARY)
 
-x = 24
-dim = (x, x)
-resize24 = cv2.resize(thresh, dim)
-retValV2, thresh24 = cv2.threshold(resize24, 120, 255, cv2.THRESH_BINARY)
+# second threshold for 24 x 24 image
+dim24 = (24, 24)
+resize24 = cv2.resize(mapped, dim24)
+imgStacks.append(("resize24x24", resize24))
 
+
+retValV2, thresh24 = cv2.threshold(resize24, 80, 255, cv2.THRESH_BINARY)
+imgStacks.append(("thresh24x24", thresh24))
+
+# final threshold for 4 x 4 image
 dim4 = (4,4)
 resize4 = cv2.resize(thresh24, dim4)
+imgStacks.append(("resize4x4", resize4))
 
 ret, thresh4 = cv2.threshold(resize4, 150, 255, cv2.THRESH_BINARY)
+imgStacks.append(("thresh4x4", thresh4))
+
+img24 = thresh24
+img4 = thresh4
 
 
 # Predicting image
-images = MyImage(dim)
+images = MyImage(dim24)
 
 print("=== prediction results ===")
 results, key, predicted = images.predict(thresh24, int(0.7 * thresh24.size))
@@ -53,41 +68,17 @@ print("The image found is", end=" ")
 print(predicted)
 
 if predicted != "error":
-    orig = images.getOriginal(predicted)
-    
+    perfectImg = images.getPerfect(predicted)
+    perfect = "prefect " + predicted.capitalize() + " image24x24"
+    imgStacks.append((perfect, perfectImg))
 
 # pyplot give number of rows, columns and index
-rows = 3
 cols = 3
+rows = math.ceil(len(imgStacks) / cols)
 pos = (rows*100) + (cols*10)
 
-plt.subplot(pos+1),plt.imshow(image, cmap = 'gray')
-plt.title('original'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(pos+2),plt.imshow(rotated, cmap = 'gray')
-plt.title('rotated'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(pos+3),plt.imshow(mapped, cmap = 'gray')
-plt.title('after mapping'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(pos+4),plt.imshow(thresh, cmap = 'gray')
-plt.title('thresh'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(pos+5),plt.imshow(resize24, cmap = 'gray')
-plt.title('resize24x24'), plt.xticks([]), plt.yticks([])
- 
-plt.subplot(pos+6),plt.imshow(thresh24, cmap = 'gray')
-plt.title('thresh24x24'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(pos+7),plt.imshow(resize4, cmap = 'gray')
-plt.title('resize4x4'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(pos+8),plt.imshow(thresh4, cmap = 'gray')
-plt.title('thresh4x4'), plt.xticks([]), plt.yticks([])
-
-if predicted != "error":
-    perfect = "prefect " + predicted.capitalize() + " image24x24"
-    plt.subplot(pos+9),plt.imshow(orig, cmap = 'gray')
-    plt.title(perfect), plt.xticks([]), plt.yticks([])
+for i in range(len(imgStacks)):
+    plt.subplot(pos+i+1),plt.imshow(imgStacks[i][1], cmap = 'gray')
+    plt.title(imgStacks[i][0]), plt.xticks([]), plt.yticks([]) 
 
 plt.show()

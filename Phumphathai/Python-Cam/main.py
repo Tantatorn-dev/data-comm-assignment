f92156b63b083f1c8c8a37229dff342b217a3198
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-11-22 19:46:58 +0700

    refactor python file

--- Phumphathai/Python-Cam/main.py
+++ Phumphathai/Python-Cam/main.py
@@ -1,73 +1,16 @@
-import serial
 import time
 import os
-import numpy as np
-import cv2
 
-
-class Camera:
-    def __init__(self, port='COM3', height=160, width=120):
-        self.COMMAND = ['*', 'R', 'D', 'Y', '*']
-
-        self.height = height
-        self.width = width
-
-        self.serial = serial.Serial()
-        self.serial.port = port
-        self.serial.baudrate = 1000000
-        self.serial.bytesize = serial.EIGHTBITS
-        self.serial.stopbits = serial.STOPBITS_ONE
-        self.serial.parity = serial.PARITY_NONE
-        self.serial.timeout = 0.2
-
-    def connect(self):
-        self.serial.open()
-        while not self.isImageStart():
-            pass
-        print("finish init camera")
-
-    def read(self):
-        raw = self.serial.read()
-        if (len(raw) != 1):
-            return False
-        return int.from_bytes(raw, "big") & 0xFF
-
-    def isImageStart(self, index=0):
-        if index < len(self.COMMAND):
-            a = self.read()
-            if a == False or a >= 128:
-                return False
-            a = chr(a)
-            if self.COMMAND[index] == a:
-                return self.isImageStart(index + 1)
-            else:
-                return False
-        return True
-
-    def getImg(self):
-        self.serial.write([ord('A')])
-
-        while not self.isImageStart():
-            pass
-
-        img_arr = np.zeros([self.height, self.width, 1], dtype=np.uint8)
-
-        for x in range(img_arr.shape[1]):
-            for y in range(img_arr.shape[0]):
-                temp = self.read()
-                if temp == False:
-                    return False
-                img_arr[y, self.width - x - 1] = [temp]
-
-        cv2.destroyAllWindows()
-        cv2.imshow('a', img_arr)
-        cv2.waitKey(10)
-
-        return img_arr
-
+from camera import Camera
 
 cam = Camera('COM3')
-time.sleep(2)
 cam.connect()
+time.sleep(2)
+i = 0
 while True:
-    cam.getImg()
+    img, Err = cam.getImg()
+    cam.display(img)
+    if not Err:
+        cam.save(img, 'out\\' + str(i) + '.jpg')
+        i = i+1
+        # time.sleep(5)

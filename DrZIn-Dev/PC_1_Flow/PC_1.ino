c04ad66cbc6d1390929f653fbd0c3079c99d7476
DrZIn <pasawee.dev@gmail.com>
2019-11-23 11:28:52 +0700

    receive data
    - add receive one data
    - add receive begin
    - is error

--- DrZIn-Dev/PC_1_Flow/PC_1.ino
+++ DrZIn-Dev/PC_1_Flow/PC_1.ino
@@ -7,12 +7,29 @@
 // TODO: Test FM Begin
 // TODO: Test Sent one Data
 // TODO: Test sentFrame
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
 
 #include <Arduino.h>
 #include <Wire.h>
 #include <FastCRC.h>
 
 FastCRC8 CRC8;
+
+// ! Receive define
+#define r_slope 20
+int prev = 0;
+int check2 = false;
+int count = 0;
+uint32_t startFreq = 0;
+char data = 0;
+uint8_t bitC = 0;
+// ! Receive define
+
 // ! FM define
 #define NUM_SAMPLE 4
 #define NUM_FREQ 4
@@ -67,13 +84,16 @@ void wireBegin();              // !
 void initFreq();               // !
 void initSinDac();             // !
 void FM_Begin();               // !
-void ReceiveOneData();
+uint8_t ReceiveOneData();      //!
+void Receive_Begin();          //!
+int8_t zone(uint16_t val);     //!
 // ! in comment => implemented
 void setup()
 {
     Serial.begin(9600);
     Serial.flush();
     //wireBegin();
+    //Receive_Begin();
     //FM_Begin();
     //userStart();
     while (startError)
@@ -170,6 +190,74 @@ void sentFrame(uint8_t *data, uint8_t size)
     }
 }
 
+void Receive_Begin()
+{
+    sbi(ADCSRA, ADPS2);
+    cbi(ADCSRA, ADPS1);
+    cbi(ADCSRA, ADPS0);
+}
+int8_t zone(uint16_t val)
+{
+    if (val <= 350)
+        return -1;
+    else if (val <= 600)
+        return 0;
+    else
+        return 1;
+}
+
+uint8_t ReceiveOneData()
+{
+    uint8_t temp;
+    while (true)
+    {
+        int8_t tmpZone = zone(analogRead(A2));
+        if (tmpZone != prev)
+        {
+            if (tmpZone == 1 && prev != 1)
+            {
+                if (check2 == false)
+                {
+                    startFreq = micros();
+                }
+                else if (micros() - startFreq > 8000)
+                {
+                    startFreq = micros();
+                    count = 0;
+                    data = 0;
+                    bitC = 0;
+                }
+            }
+            if (tmpZone == 0 && prev == 1)
+            {
+                count++;
+                check2 = true;
+            }
+            if (tmpZone == 0 && prev == -1)
+            {
+                if (check2 == true && micros() - startFreq > 3500)
+                {
+                    data <<= 2;
+                    data |= (count - 2) & B0011;
+                    bitC++;
+                    if (bitC == 4)
+                    {
+                        Serial.print(data);
+                        temp = data;
+                        data = 0;
+                        bitC = 0;
+                        break;
+                    }
+                    check2 = false;
+                    count = 0;
+                }
+            }
+            prev = tmpZone;
+        }
+    }
+    return temp;
+}
+
 void ReceiveData()
 {
 }
@@ -180,6 +268,22 @@ void printData()
 
 int isError()
 {
+    uint8_t count = 0;
+    uint8_t errorData;
+    while (count != uint8_t(2))
+    {
+        uint8_t temp = ReceiveOneData();
+        if (temp == flag)
+        {
+            count++;
+        }
+        else
+        {
+            errorData = temp;
+            break;
+        }
+    }
+    return uint8_t(errorData);
 }
 
 void clearData()

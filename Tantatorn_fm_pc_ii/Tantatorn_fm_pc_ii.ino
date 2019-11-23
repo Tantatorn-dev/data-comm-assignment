e47d28a5171f69f18584ff9d27372676ac91dab7
Tantatorn <timefear007@gmail.com>
2019-11-23 13:40:57 +0700

    create class for FM_Rx

--- Tantatorn_fm_pc_ii/Tantatorn_fm_pc_ii.ino
+++ Tantatorn_fm_pc_ii/Tantatorn_fm_pc_ii.ino
@@ -1,55 +1,25 @@
-// defines for setting and clearing register bits
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
-
-#include <Wire.h>
-
-#include <TEA5767Radio.h>
-TEA5767Radio radio = TEA5767Radio();
+#include "pc_ii_state.h"
+#include "FM_Rx.h"
 
 #include <Servo.h>
 Servo servoPan;
 Servo servoTilt;
 
-#define r_slope 20
+FM_Rx * receiver;
+
 void setup() {
-  // put your setup code here, to run once:
-  sbi(ADCSRA, ADPS2);
-  cbi(ADCSRA, ADPS1);
-  cbi(ADCSRA, ADPS0);
 
   Serial.begin(115200);
   Serial.flush();
 
-  Wire.begin();
-  radio.setFrequency(93.44);
+  receiver = new FM_Rx();
 
+  Wire.begin();
+  
   initServo();
 }
 
-// Defined enum
-enum pc2_fm_state {
-  AWAITING_PC1,
-  AWAITING_PC2,
-  SENDING_PC1
-};
-
 // Global variables
-
-int prev = 0;
-int check2 = false;
-int count = 0;
-
-uint32_t startFreq = 0;
-
-char data_pc1 = 0;
-uint8_t bitC = 0;
-
-pc2_fm_state state = AWAITING_PC1;
 
 char data_pc2 = 0;
 char  pos[3];
@@ -60,7 +30,8 @@ void loop() {
 
   if (state == AWAITING_PC1) {
     // awaiting commands from PC1
-    receiveFM();
+    if(receiver->receiveFM() == 's') 
+    state = AWAITING_PC2;
   }
   else if (state == AWAITING_PC2) {
     // rotate camera and receive values
@@ -81,44 +52,6 @@ void initServo() {
 
   servoTilt.write(50);
   servoPan.write(70);
-}
-
-void receiveFM() {
-  int8_t tmpZone = zone(analogRead(A2));
-  if (tmpZone != prev) {
-    if (tmpZone == 1 && prev != 1) {
-      if (check2 == false) {
-        startFreq = micros();
-      } else if (micros() - startFreq > 8000) {
-        startFreq = micros();
-        count = 0;
-        data_pc1 = 0;
-        bitC = 0;
-      }
-    }
-    if (tmpZone == 0 && prev == 1) {
-      count++;
-      check2 = true;
-    }
-    if (tmpZone == 0 && prev == -1) {
-      if (check2 == true && micros() - startFreq > 3500) {
-        data_pc1 <<= 2;
-        data_pc1 |= (count - 2) & B0011;
-        bitC++;
-        if (bitC == 4) {
-          // Serial.print((char)data_pc1);
-          if (data_pc1 == 's') {   
-            state = AWAITING_PC2;
-          }
-          data_pc1 = 0;
-          bitC = 0;
-        }
-        check2 = false;
-        count = 0;
-      }
-    }
-    prev = tmpZone;
-  }
 }
 
 
@@ -185,8 +118,6 @@ void rotate_camera(char direction_camera) {
   }
 }
 
-int8_t zone(uint16_t val) {
-  if (val <= 350) return -1;
-  else if (val <= 600) return 0;
-  else return 1;
+void sendFM(){
+  
 }

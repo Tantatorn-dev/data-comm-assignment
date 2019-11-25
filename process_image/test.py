83c6e58843badb43d61d5b535f8c7643873d2e76
Kittisak <dnjooiopa@gmail.com>
2019-11-25 18:15:07 +0700

    get 16 coordinates

--- process_image/test.py
+++ process_image/test.py
@@ -1,12 +1,23 @@
 import os
 
-
 import numpy as np
+import cv2
 
+cor16 = np.array([
+         [[25, 20, 0], [25,40, 0], [25, 60, 0], [25, 80, 0]],
+        [[55, 20, 0], [55,40, 0], [55, 60, 0], [55, 80, 0]],
+        [[105, 20, 0], [105,40, 0], [105, 60, 0], [105, 80, 0]],
+        [[135, 20, 0], [135,40, 0], [135, 60, 0], [135, 80, 0]],
+        ])
 
-a = np.zeros((10, 10))
+rows, cols, _ = cor16.shape
 
-r, c = a.shape
+img = cv2.imread("C:\\out\\2.jpg", 0)
 
-print(r, c)
+for i in range(rows):
+    for j in range(cols):
+        y = cor16[i][j][0]
+        x = cor16[i][j][1]
+        cor16[i][j][2] = img[y][x]
 
+print(cor16)

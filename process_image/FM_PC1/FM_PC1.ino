23f0b1f50817806badfac44bf69c623c47c1d038
Kittisak <dnjooiopa@gmail.com>
2019-11-25 22:47:11 +0700

    1st refactor of FM_PC1

--- process_image/FM_PC1/FM_PC1.ino
+++ process_image/FM_PC1/FM_PC1.ino
@@ -2,30 +2,29 @@
 #include "FM_Tx.h"
 #include <String.h>
 
-FM_Rx * receiver;
-FM_Tx * transmitter;
+FM_Rx *receiver;
+FM_Tx *transmitter;
 
 //------ Global Variable -----
 enum STATES
 {
-  START = 1, GET_3DATA, LAST_STATE
-};
-
-enum STATES state  = START;
+  START,
+  GET_3DATA,
+  LAST_STATE
+} state = START;
 
 String lable[6] = {"Top", "Bottom", "Left", "Right", "Upper", "Lower"};
-int8_t angle[3] = { -45, 0, 45};
+int8_t angle[3] = {-45, 0, 45};
 char pos[3];
-uint8_t raw48[48];
 struct Cor16
 {
   uint8_t x;
   uint8_t y;
   uint8_t color;
-} cor16[16];
+} points16[16];
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.flush();
 
@@ -33,58 +32,70 @@ void setup() {
   transmitter = new FM_Tx();
 }
 
-String getImageType(char key) {
+String getImageType(char key)
+{
   int lableIndex = int(key) - 49;
   return lable[lableIndex];
 }
 
-void showAll() {
-  Serial.println("--Data received from PC2--");
-  for (int j = 0; j < 3; j++) {
+void showAll()
+{
+  Serial.println("-- Data of 3 images received from PC2 --");
+  for (int j = 0; j < 3; j++)
+  {
     String imgType = getImageType(pos[j]);
-    Serial.println(String(j + 1) + ". " + imgType + " at " + String(angle[j]) + " degree");
+    String imgReceive = String(j + 1) + ". " + imgType + " at " + angle[j] + " degree";
+    Serial.println(imgReceive);
   }
   Serial.println();
 }
 
-void start() {
+void start()
+{
   Serial.println("======= start program =======");
-  while (state == START) {
+  while (state == START)
+  {
     Serial.print("Press \'s\' to get 3 image types from PC2: ");
-    while (!Serial.available());
+    while (!Serial.available())
+      ;
     char in = Serial.read();
     Serial.println(in);
-    if (in == 's') {
+    if (in == 's')
+    {
       Serial.println("Sending \'s\' to PC2...");
       transmitter->sendFM(in);
       state = GET_3DATA;
     }
-    else {
+    else
+    {
       Serial.println("wrong input please try again!\n");
     }
   }
-
 }
 
-
-void get3DataFromPC2() {
+void get3ImageData()
+{
   Serial.println("Getting 3 data from PC2...");
-  uint32_t t = millis();
-  int i = 0;
-  while (i < 3) {
+  uint32_t timeout = millis();
+  int index = 0;
+  while (index < 3)
+  {
     int receive = receiver->receiveFM();
-    if (receive != -1) {
+    if (receive != -1)
+    {
       Serial.print(char(receive));
       Serial.print(" ");
-      pos[i++] = char(receive);
-      if (i == 3) {
+      pos[index++] = char(receive);
+      if (index == 3)
+      {
         Serial.println();
         showAll();
         state = LAST_STATE;
         delay(300);
       }
     }
-    if (millis() - t > 10000) {
+    if (millis() - timeout > 10000)
+    {
       Serial.println("Time out of 10 seconds");
       state = START;
       break;
@@ -92,99 +103,98 @@ void get3DataFromPC2() {
   }
 }
 
-void showAllPoints(char key) {
-  int indexShow = 0;
-  Serial.println("Display 16 points of an " + getImageType(key) + " image");
-  for (int j = 0; j < 16; j++) {
-    for (int k = 0; k < 3; k++) {
-      Serial.print(String(raw48[indexShow++]));
-      Serial.print(" ");
-    }
-    Serial.println();
-  }
-}
-
-void showStruct(char key) {
+void showPoints16(char key)
+{
   Serial.println("Display 16 points in struct of an " + getImageType(key) + " image");
-  for (int i = 0; i < 16; i++) {
-    Serial.print("x = ");
-    Serial.print(cor16[i].x);
-    Serial.print(", y = ");
-    Serial.print(cor16[i].y);
-    Serial.print(", color = ");
-    Serial.println(cor16[i].color);
+  for (int i = 0; i < 16; i++)
+  {
+    String pointData = "x = " + String(points16[i].x) + ", y = " + String(points16[i].y) + ", color = " + String(points16[i].color);
+    Serial.println(pointData);
   }
-
 }
 
-void lastState() {
-  while (state == LAST_STATE) {
+void lastState()
+{
+  while (state == LAST_STATE)
+  {
     Serial.print("Choose Image ==> ");
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
       char key = pos[i];
       String imgType = getImageType(key);
-      Serial.print("[");
-      Serial.print(key);
-      Serial.print("]->");
-      Serial.print(imgType);
-      Serial.print(" ");
+      String imgPrint = "[" + String(key) + "]->" + imgType + " ";
+      Serial.print(imgPrint);
     }
     Serial.println(" [r]->reset program");
     Serial.print("Input : ");
-    while (!Serial.available());
+    while (!Serial.available())
+      ;
     char in = Serial.read();
     Serial.println(in);
-    if (in == pos[0] or in == pos[1] or in == pos[2] or in == 'r') {
-      Serial.print("Sending ");
-      Serial.print(in);
-      Serial.println(" to PC2...");
+    if (in == pos[0] or in == pos[1] or in == pos[2] or in == 'r')
+    {
+      String sendingStatus = "Sending " + String(in) + " to PC2...";
+      Serial.print(sendingStatus);
       transmitter->sendFM(in);
-      if (in == 'r') {
+      if (in == 'r')
+      {
         Serial.println("reset program");
         Serial.println();
         state = START;
         break;
       }
-      uint32_t ti = millis();
-      int indexRaw = 0;
-      while (indexRaw != 48) {
-        if (millis() - ti > 15000) {
-          Serial.println("Time out of 15 seconds");
-          memset(raw48, 0, 48);
+      uint32_t timeout = millis();
+      uint8_t rawIndex = 0;
+      while (rawIndex < 48)
+      {
+        if (millis() - timeout > 10000)
+        {
+          Serial.println("Time out of 10 seconds");
           break;
         }
         int temp = receiver->receiveFM();
-        if (temp != -1) {
-          raw48[indexRaw] = temp;
-          int corIndex = indexRaw / 3;
-          if (indexRaw % 3 == 0) cor16[corIndex].y = temp;
-          else if (indexRaw % 3 == 1)cor16[corIndex].x = temp;
-          else cor16[corIndex].color = temp;
-          indexRaw++;
+        if (temp != -1)
+        {
+          int pIndex = rawIndex / 3;
+          if (rawIndex % 3 == 0)
+            points16[pIndex].y = temp;
+          else if (rawIndex % 3 == 1)
+            points16[pIndex].x = temp;
+          else
+            points16[pIndex].color = temp;
+          rawIndex++;
         }
       }
-      if (indexRaw == 48) {
-        showAllPoints(in);
-        showStruct(in);
-      } else {
+      if (rawIndex == 48)
+      {
+        showPoints16(in);
+      }
+      else
+      {
         Serial.println("Data lost!! May be something wrong.");
       }
     }
-    else {
+    else
+    {
       Serial.println("wrong input please try again!");
       Serial.println();
     }
   }
 }
 
-
-void loop() {
-  if (state == START) {
+void loop()
+{
+  if (state == START)
+  {
     start();
-  } else if (state == GET_3DATA) {
-    get3DataFromPC2();
-  } else if (state == LAST_STATE) {
+  }
+  else if (state == GET_3DATA)
+  {
+    get3ImageData();
+  }
+  else if (state == LAST_STATE)
+  {
     lastState();
   }
 }
